219d529fd373a5a89ced55eb1dccaf14f88a5e3e
dmly0217 <dmytro.lysai@netcracker.com>
2021-04-16 17:05:14 +0300

    csrf added

--- src/main/java/com/example/demo/app/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/app/security/ApplicationSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -28,14 +29,12 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                //.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(UserRole.USER.name())
-                //.antMatchers(HttpMethod.DELETE,"/management/api/**").hasAnyAuthority(UserPermission.COURSE_WRITE.getPermission())
-                //.antMatchers(HttpMethod.POST,"/management/api/**").hasAnyAuthority(UserPermission.COURSE_WRITE.getPermission())
-                //.antMatchers(HttpMethod.PUT,"/management/api/**").hasAnyAuthority(UserPermission.COURSE_WRITE.getPermission())
-                //.antMatchers("/management/api/**").hasAnyRole(UserRole.ADMIN.name(), UserRole.ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
